2a4afa9bdd8c271ab3bfebc29044bb185c26ca09
Brody14 <stefania.spinas@gmail.com>
2023-02-15 15:48:13 +0100

    FEAT dom element

--- js/script.js
+++ js/script.js
@@ -1,40 +1,71 @@
-console.log('Final-8')
+//console.log('Final-8');
+
+const carouselEl = document.querySelector(".carousel");
 
 const slides = [
-    {
-        imgSrc : './img/01.png',
-        title : 'Final Eight 2023',
-        description : 'Qualche curiosità sulla coppa (giusto per renderci conto che Milano non è così figa come finge di essere',
-    },
+	{
+		imgSrc: "./img/01.png",
+		title: "Final Eight 2023",
+		description:
+			"Qualche curiosità sulla coppa (giusto per renderci conto che Milano non è così figa come finge di essere)",
+	},
 
-    {
-        imgSrc : './img/02/jpg',
-        title : 'Benetton Treviso',
-        description : 'La squadra che ha vinto il maggior numero di coppe, 5, una in più di Milano!',
-    },
+	{
+		imgSrc: "./img/02.jpg",
+		title: "Benetton Treviso",
+		description:
+			"La squadra che ha vinto il maggior numero di coppe, 5, una in più di Milano!",
+	},
 
-    {
-        imgSrc : './img.03.jpg',
-        title : 'Olimpia Milano',
-        description : 'La squadra che ha partecipato a più Final Eight (ben 18 edizioni, pensate la noia) per vincerne solo 4!',
-    },
+	{
+		imgSrc: "./img/03.jpg",
+		title: "Olimpia Milano",
+		description:
+			"La squadra che ha partecipato a più Final Eight (ben 18 edizioni, pensate la noia) per vincerne solo 4!",
+	},
 
-    {
-        imgSrc : './img.04.jpg',
-        title : 'Sassari - Pesaro, Finale Eight 2021',
-        description : 'La partita (stupenda) con il punteggio più alto nella storia delle Final Eight con 225 punti totali',
-    },
+	{
+		imgSrc: "./img/04.jpg",
+		title: "Sassari - Pesaro, Finale Eight 2021",
+		description:
+			"La partita (stupenda) con il punteggio più alto nella storia delle Final Eight con 225 punti totali",
+	},
 
-    {
-        imgSrc : './img/05.jpg',
-        title : 'Ettore Messina',
-        description : "L'allenatore che ha vinto più coppe nella storia, nel 7 (il fatto che alleni Milano è solo un caso)",
-    },
+	{
+		imgSrc: "./img/05.jpg",
+		title: "Ettore Messina",
+		description:
+			"L'allenatore che ha vinto più coppe nella storia, nel 7 (il fatto che alleni Milano è solo un caso)",
+	},
 
-    {
-        imgSrc : './img.06.jpg',
-        title : 'Adrian Banks',
-        description : 'Ha realizzato il maggior numero di punti della storia delle Final Eight in una singola partita (37 - Final Eight 2020) e indovinate? Non giocava a Milano!',
-    },
-]
+	{
+		imgSrc: "./img/06.jpg",
+		title: "Adrian Banks",
+		description:
+			"Ha realizzato il maggior numero di punti della storia delle Final Eight in una singola partita (37 - Final Eight 2020) e indovinate? Non giocava a Milano!",
+	},
+];
 
+for (let i = 0; i < slides.length; i++) {
+	let slide = slides[i];
+	const imgSrc = slide.imgSrc;
+	const title = slide.title;
+	const description = slide.description;
+
+	let slideEl = document.createElement("div");
+	slideEl.classList.add("slide");
+
+	if (i === 0) {
+		slideEl.classList.add("active");
+	}
+
+	slideEl.innerHTML += `<img src=${imgSrc}>`;
+
+	let slideBodyEl = document.createElement("div");
+	slideBodyEl.classList.add("slide-body");
+	slideBodyEl.innerHTML += `<h2>${title}</h2> <p>${description}</p>`;
+
+	slideEl.appendChild(slideBodyEl);
+
+	carouselEl.appendChild(slideEl);
+}
